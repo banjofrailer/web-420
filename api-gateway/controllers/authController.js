6e6fe7767b287503700ff07d0d48737874dd3944
Sarah Kovar <sarahkovar@gmail.com>
2020-06-21 22:59:15 -0500

    week 8

--- api-gateway/controllers/authController.js
+++ api-gateway/controllers/authController.js
@@ -42,14 +42,14 @@ exports.user_register = function(req, res) {
 //Verify token on GET
 exports.user_token = function(req, res) {
     
-    var token = req.headers['x-access-token'];
+//    var token = req.headers['x-access-token'];
 
-    if (!token) return res.status(401).send({ auth: false, message: 'No token provided'});
+//    if (!token) return res.status(401).send({ auth: false, message: 'No token provided'});
 
-    jwt.verify(token, config.web.secret, function (err, decoded)  {
-        if (err) return resizeTo.status(500).send({auth: false, message: 'Failed to authenticate token.'});
+//    jwt.verify(token, config.web.secret, function (err, decoded)  {
+//        if (err) return resizeTo.status(500).send({auth: false, message: 'Failed to authenticate token.'});
 
-        User.getById(decoded.id, function(err, user)  {
+        User.getById(req.userId, function(err, user)  {
             if (err) return res.status(500).send('There was a problem finding the user.');
 
             if (!user) return res.status(404).sent('No user found.');
@@ -57,10 +57,10 @@ exports.user_token = function(req, res) {
             res.status(200).send(user);
         });
 
-    });
+    };
 
     //res.send('NOT IMPLEMENTED: User token lookup GET');
-};
+
 
 //function to handle user login requests
 exports.user_login = function(req, res) {
--- api-gateway/routes/api-catalog.js
+++ api-gateway/routes/api-catalog.js
@@ -11,6 +11,7 @@
 var express = require('express');
 var router = express.Router();
 var auth_controller = require('../controllers/authController');
+var checkToken = require('../check-token')
 
 //POST request for registering a user
 router.post('/auth/register', auth_controller.user_register);
@@ -19,7 +20,7 @@ router.post('/auth/register', auth_controller.user_register);
 router.post('/auth/login', auth_controller.user_login);
 
 //GET request for verifying user tokens
-router.get('/auth/token', auth_controller.user_token);
+router.get('/auth/token', checkToken, auth_controller.user_token);
 
 //GET request for allowing user logout requests
 router.get('/auth/logout', auth_controller.user_logout);
